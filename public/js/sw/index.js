0e176bc09e94c3342ea3438350a3b2118c67fc69
Ruto Collins <borecollins@gmail.com>
2018-05-16 23:01:00 +0300

    Request hijacking 2

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,20 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+	// url ending in ".jpg"
+
+	// let url = '/imgs/dr-evil.gif'
+
+	// if ((/\.(gif)$/i).test(url)) {
+	// 	event.respondWith(
+	// 		fetch(url)
+	// 	);
+	// } else {
+	// 	console.log('Invalid URL that is!');
+	// }
+
+	if (event.request.url.endsWith('.jpg')) {
+		event.respondWith(
+			fetch('/imgs/dr-evil.gif')
+		);
+	}
 });

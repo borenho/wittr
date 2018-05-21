755408d4ef93eb1aa5278e5d1c4af3e691984a3f
Ruto Collins <borecollins@gmail.com>
2018-05-21 21:56:12 +0300

    Show a dummy notification

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,5 +1,5 @@
 var staticCacheName = 'wittr-static-v2';
-
+// boolCheck01qa
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
